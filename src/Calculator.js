12c22b380878b47bc140969e482e7da9fed84f55
James Sinkler <jamessinkler713@gmail.com>
2021-02-24 20:41:37 -0800

    Steps 2-5: Addition works

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,18 +1,41 @@
 import { Component } from 'react';
 
 class Calculator extends Component {
+  state = {
+    num1: NaN,
+    num2: NaN,
+    sum: NaN
+  }
+
+  setNum = (e) => {
+      this.setState({ [e.target.name]: Number(e.target.value) })
+  }
+
+  getSum = ()=> {
+    this.setState({ sum: this.state.num1 + this.state.num2 })
+  }
 
   render() {
-
     return(
       <div className="container">
         <h1>Add with React!</h1>
         <div className="add">
-          <input type="text" />
+          <input type="number"
+            name="num1"
+            placeholder="Enter your first number"
+            value={this.state.num1}
+            onChange={this.setNum}
+          />
           <span>+</span>
-          <input type="text" />
-          <button>=</button>
-          <h3>Addition results go here!</h3>
+          <input type="number"
+            name="num2"
+            placeholder="Enter your first number"
+            value={this.state.num2}
+            onChange={this.setNum}
+          />
+          <button onClick={this.getSum}>=</button>
+          <h3>Addition results go here!</h3> 
+          <p>{ isNaN(this.state.sum) ? 'Add two numbers' : this.state.sum }</p>
         </div>
       </div>
     )
